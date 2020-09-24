e20542edad478ab48f4acaa9dd04f7c0c49377a6
Wijmar Snijder <snijderwijmar@gmail.com>
2020-09-24 15:53:15 +0200

    test

--- Homework Exercises.py
+++ Homework Exercises.py
@@ -14,3 +14,24 @@ print(alle + werk + en + geen + spelen +maakt+hem+een+saaie+gozer)
 
 #2
 
+print(6*1-2)
+print(6*(1-2))
+
+#3
+
+
+#4
+
+bruce = 6
+int(bruce)
+print(bruce + 4)
+
+#5
+
+#6
+
+#7
+
+#8
+a=14+51%24
+print(a)
