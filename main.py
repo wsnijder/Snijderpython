b840bc0005cb889f03a3f8f2bdffcf65daba9db2
Wijmar Snijder <snijderwijmar@gmail.com>
2020-09-24 12:53:10 +0200

    test

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+#Get information
+ask_user = float(input("Please give me a number"))
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#Do calculation
+calculated_answer = ask_user + 2
 
+#give output
+print (ask_user + 2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
