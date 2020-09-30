5f8403d2689f091a7d2b7f6abb7a72be9afbef57
Wijmar Snijder <snijderwijmar@gmail.com>
2020-09-30 11:55:37 +0200

    Ferdi g

--- Homework Exercises.py
+++ Homework Exercises.py
@@ -11,7 +11,7 @@ een = " a"
 saaie = " dull"
 gozer = " boy"
 print(alle + werk + en + geen + spelen +maakt+hem+een+saaie+gozer)
-
+print(alle, werk, en, geen, spelen, maakt, hem, een, saaie, gozer)
 #2
 
 print(6*1-2)
@@ -27,11 +27,22 @@ int(bruce)
 print(bruce + 4)
 
 #5
+principal_amount = float(input("What is the principal amount?"))
+frequency =
+interest_rate =
+duration =
 
 #6
 
 #7
-
-#8
 a=14+51%24
 print(a)
+
+#8
+
+current_time = int(input("hat is the current time?"))
+alarm_remaining = int(input("How long do you have to wait?"))
+alarm_goes_off = (current_time + alarm_remaining) %24
+
+print("the alarm goes off at", alarm_goes_off)
+
